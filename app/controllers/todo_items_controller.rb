6b1b330cc24a612b530f586c4f10cc288814fe92
leon-wee <leonwee27@gmail.com>
2016-10-09 13:35:03 -0400

    Added todo and todo_items

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,31 @@
+class TodoItemsController < ApplicationController
+   before_action :set_todo
+
+
+   def create
+      @todo_item = @todo.todo_items.create(todo_item_params)
+      
+      redirect_to @todo
+   end
+   
+   def destroy
+      @todo_item = @todo.todo_items.find(params[:id])
+      if @todo_item.destroy
+         flash[:success] = "Todo list item was deleted."
+      else
+         flash[:error] = "Todo List item could not be deleted."
+      end
+      redirect_to @todo
+   end
+
+
+   private
+
+   def set_todo
+      @todo = Todo.find(params[:todo_id])
+   end
+ 
+   def todo_item_params
+      params[:todo_item].permit(:content)
+   end
+end
